ce461200eec40e92951b6554be2b71e3c32229ea
LilyanAhmet <l.ahmetoglu@qimia.de>
2021-11-29 13:37:23 +0300

    adding the video

--- src/components/MultipleVideos.js
+++ src/components/MultipleVideos.js
@@ -1,56 +1,141 @@
 import React from "react";
-import ReactPlayer from "react-player";
-
-const MultipleVideos = props => {
-
-    return (
-        <div className="video-wrapper">
-            <div className="row mx-0">
-                <div className="col-12 col-md-6 col-xl-3 px-0">
-                <ReactPlayer
-                    width="100%"
-                    controls={true}
-                    className="mx-auto"
-                    url="https://aws-3d-tanbur-bucket.s3.eu-central-1.amazonaws.com/tanbur_1.mov"
-                />
-                </div>
-                <div className="col-12 col-md-6 col-xl-6 px-0">
-                <ReactPlayer
-                    width="100%"
-                    controls={true}
-                    className="mx-auto"
-                    url="https://aws-3d-tanbur-bucket.s3.eu-central-1.amazonaws.com/601417505.mp4"
-                />
-                </div>
-                <div className="col-12 col-md-12 col-xl-3 px-0">
-                <ReactPlayer
-                    url="https://aws-3d-tanbur-bucket.s3.eu-central-1.amazonaws.com/The+Art+of+Ostad+Elahi.mp4"
-                    width="100%"
-                    controls={true}
-                    className="mx-auto"
-                    />
-                </div>
-            </div>
-            <div className="row mx-0">
-                <div className="col-12 col-md-6 col-xl-7 px-0">
-                <ReactPlayer
-                    url={'https://www.youtube.com/watch?v=bC8EmPA6H6g&ab_channel=Moshcam'}
-                    width="100%"
-                    controls={true}
-                    className="mx-auto"
-                    />
-                </div>
-                <div className="col-12 col-md-6 col-xl-5 px-0">
-                <ReactPlayer
-                    url={'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark'}
-                    width="100%"
-                    controls={true}
-                    className="mx-auto"
-                    />
-                </div>
-            </div>
+import HoverVideoPlayer from "react-hover-video-player";
+import Tanbur_3string from "../resources/img/tanbur-3string-img.png";
+/* resources */
+import Place1 from '../resources/img/placeholder-1.png'
+import Place2 from '../resources/img/placeholder-2.png'
+import Place3 from '../resources/img/placeholder-3.png'
+import Place4 from '../resources/img/placeholder-4.png'
+import Place5 from '../resources/img/placeholder-5.png'
+const MultipleVideos = (props) => {
+  return (
+    <div className="video-wrapper">
+      <div className="row mx-0">
+        <div className="col-12 col-md-6 col-xl-4 px-0">
+          <HoverVideoPlayer
+            videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/6EpNv5uYLX70WDOkXaUmhM/e5fe245f197b1ba4021f9ab78cddfa7e/tanbur_1.mp4"
+            className="video-gallery"
+            pausedOverlay={
+              <img
+                src={Place1}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+            controls
+          />
         </div>
-    )
-}
+        <div className="col-12 col-md-6 col-xl-4 px-0">
+          <HoverVideoPlayer
+            videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/2Df4hXKKnGOAMDsPhzLK4p/868a3d7deaf046b2eab9fd816e3552b8/tanbur_2.mp4"
+            className="video-gallery"
+            pausedOverlay={
+              <img
+                src={Place2}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+            controls
+          />
+        </div>
+        <div className="col-12 col-md-12 col-xl-4 px-0">
+          <HoverVideoPlayer
+            videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/3yyUMz9ohAem6dCTuYmV77/ed51f1bf17eeb370c9930ed1f0ee7e53/tanbur_3.mp4"
+            className="video-gallery"
+            pausedOverlay={
+              <img
+                src={Place3}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+            controls
+          />
+        </div>
+      </div>
+      <div className="row mx-0">
+        <div className="col-12 col-md-6 col-xl-6 px-0">
+          <HoverVideoPlayer
+            videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/sFr1CLaPvD7RNiXhzNe9G/0ed05a86f271eebfca9cba6a99d4544b/tanbur_4.mp4"
+            className="video-gallery"
+            pausedOverlay={
+              <img
+                src={Place4}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+            controls
+          />
+        </div>
+        <div className="col-12 col-md-6 col-xl-6 px-0">
+          <HoverVideoPlayer
+            videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/5f9UmdFnyufz6I1QK94rAs/bd80bb644eef403a3435c28128cfbee7/tanbur_5.mp4"
+            className="video-gallery"
+            pausedOverlay={
+              <img
+                src={Place5}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            }
+            loadingOverlay={
+              <div className="loading-overlay">
+                <div className="loading-spinner" />
+              </div>
+            }
+            controls
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MultipleVideos
+export default MultipleVideos;
