eb8b6a14439187f295803a1e0ee313384b533e0d
Eymen-Alpaydin <58814757+Eymen-Alpaydin@users.noreply.github.com>
2021-12-04 22:07:56 +0300

    videos are unmuted.

--- src/components/MultipleVideos.js
+++ src/components/MultipleVideos.js
@@ -12,6 +12,8 @@ const MultipleVideos = (props) => {
       <div className="row mx-0">
         <div className="col-12 col-md-6 col-xl-4 px-0">
           <HoverVideoPlayer
+            volume={1}
+            muted={false}
             videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/6EpNv5uYLX70WDOkXaUmhM/e5fe245f197b1ba4021f9ab78cddfa7e/tanbur_1.mp4"
             className="video-gallery"
             pausedOverlay={
@@ -37,6 +39,8 @@ const MultipleVideos = (props) => {
         </div>
         <div className="col-12 col-md-6 col-xl-4 px-0">
           <HoverVideoPlayer
+            volume={1}
+            muted={false}
             videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/2Df4hXKKnGOAMDsPhzLK4p/868a3d7deaf046b2eab9fd816e3552b8/tanbur_2.mp4"
             className="video-gallery"
             pausedOverlay={
@@ -62,6 +66,8 @@ const MultipleVideos = (props) => {
         </div>
         <div className="col-12 col-md-12 col-xl-4 px-0">
           <HoverVideoPlayer
+            volume={1}
+            muted={false}
             videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/3yyUMz9ohAem6dCTuYmV77/ed51f1bf17eeb370c9930ed1f0ee7e53/tanbur_3.mp4"
             className="video-gallery"
             pausedOverlay={
@@ -89,6 +95,8 @@ const MultipleVideos = (props) => {
       <div className="row mx-0">
         <div className="col-12 col-md-6 col-xl-6 px-0">
           <HoverVideoPlayer
+            volume={1}
+            muted={false}
             videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/sFr1CLaPvD7RNiXhzNe9G/0ed05a86f271eebfca9cba6a99d4544b/tanbur_4.mp4"
             className="video-gallery"
             pausedOverlay={
@@ -114,6 +122,8 @@ const MultipleVideos = (props) => {
         </div>
         <div className="col-12 col-md-6 col-xl-6 px-0">
           <HoverVideoPlayer
+            volume={1}
+            muted={false}
             videoSrc="https://videos.ctfassets.net/unrnfh6tg4kn/5f9UmdFnyufz6I1QK94rAs/bd80bb644eef403a3435c28128cfbee7/tanbur_5.mp4"
             className="video-gallery"
             pausedOverlay={
