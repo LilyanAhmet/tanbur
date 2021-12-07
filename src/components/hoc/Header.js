0f0d2fcdcdff69142bf265e9ceb42770576d0916
Eymen-Alpaydin <58814757+Eymen-Alpaydin@users.noreply.github.com>
2021-12-07 18:39:59 +0300

    font and text style changes

--- src/components/hoc/Header.js
+++ src/components/hoc/Header.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import Tanbur_Img from "../../resources/img/tanbur-banner-img.png";
+import Tanbur_Img from "../../resources/img/tanbur-banner-img.webp";
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <header>
-                    <div className="container d-table header-wrapper">
-                        <table className="mx-auto">
-                            <tr>
-                                <td><h1>TAN</h1></td>
-                                <td><img className="col-4" id="header-img" src={Tanbur_Img} alt="tanbur" /></td>
-                                <td><h1>UR</h1></td>
-                            </tr>
-                        </table>
-                    </div>
-                </header>
-            </div>
+            <header>
+                <div className="container d-table header-wrapper">
+                    <table className="mx-auto">
+                        <tr>
+                            <td><h1>TAN</h1></td>
+                            <td><img className="col-4" id="header-img" src={Tanbur_Img} alt="tanbur" /></td>
+                            <td><h1>UR</h1></td>
+                        </tr>
+                    </table>
+                </div>
+            </header>
         )
     }
 }
